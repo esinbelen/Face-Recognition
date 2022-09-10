8ef51aeaf8aa4b2993c92ae11c9a5c8a7331b8f1
esinbelen <esinbelen@hotmail.com>
2022-09-10 15:37:09 +0300

    check if the libraries are installed

---
+++ code/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 10 15:11:25 2022
+
+@author: esinb
+"""
+
+import cv2
+import dlib
+import face_recognition
+
+print(cv2.__version__)
+print(dlib.__version__)
+print(face_recognition.__version__)
